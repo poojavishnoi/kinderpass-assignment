29715ed9f9878d6adcfb2906b6fa4ed4d5c13de5
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-09-12 22:44:47 +0530

    cleaned home code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,172 +1,53 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
 import { useEmployee } from "../context/employee-context";
 import { EmployeeCard } from "../components/EmployeeCard";
-import { MdAddCircle } from "react-icons/md";
-
 import "../style/home.css";
+import EmployeeModal from "../components/EmployeeModal";
+import { Button } from "react-bootstrap";
 
 function Home() {
-
-  const { employee, dispatch } = useEmployee();
-
-  const [show, setShow] = useState(false);
-  const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
-  const [dob, setDob] = useState("");
-  const [name, setName] = useState("");
-  const [city, setCity] = useState("");
-  const [number, setNumber] = useState("");
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  const handleSave = () => {
-    dispatch({
-      type: "ADD_EMP",
-      payload: {
-        name,
-        email,
-        number,
-        dob,
-        city,
-        address,
-      },
-    });
-
-    setShow(false);
-  };
+  const { employee } = useEmployee();
+  const [toShowEmployeePopup, setToShowEmployeePopup] = useState(false)
   
+
+  const addEmployeeClickHandler = () => {
+    console.log("adding employee");
+    setToShowEmployeePopup(true)
+  }
+
+  const closeEmployeeClickHandler = () => setToShowEmployeePopup(false)
+
+  let toRender;
+
+
+  if (employee.length === 0 ) {
+    toRender = (
+      <div className="empty">
+      <p>Nothing here</p>
+      <img
+        className="image"
+        src="https://cdn.dribbble.com/users/453325/screenshots/5573953/empty_state.png"
+        alt=""
+      />
+    </div>
+    )
+  } else {
+    toRender = (
+      <div className="home_container">
+          {employee.map((data, index) => {
+            return <EmployeeCard key={index} data={data} />;
+          })}
+        </div>
+    )
+  }
+
   return (
     <>
-      <div className="add_employee_container">
-        <div className="add">
-          <MdAddCircle
-            className="card_icon add_icon"
-            onClick={handleShow}
-            size="2rem"
-          />
-          <p>Add employeeCard</p>
-        </div>
-
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Add employee details</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  onChange={(e) => {
-                    setName(e.target.value);
-                  }}
-                  type="text"
-                  autoFocus
-                />
-              </Form.Group>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                  type="email"
-                  autoFocus
-                />
-              </Form.Group>
-
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label>Date of birth :</Form.Label>
-                <Form.Control
-                  onChange={(e) => {
-                    setDob(e.target.value);
-                  }}
-                  type="date"
-                  autoFocus
-                />
-              </Form.Group>
-
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Label>Mobile No.</Form.Label>
-                <Form.Control
-                  onChange={(e) => {
-                    setNumber(e.target.value);
-                  }}
-                  type="tel"
-                  autoFocus
-                />
-              </Form.Group>
-
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Label>City</Form.Label>
-                <Form.Control
-                  onChange={(e) => {
-                    setCity(e.target.value);
-                  }}
-                  type="text"
-                  autoFocus
-                />
-              </Form.Group>
-
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label>Address</Form.Label>
-                <Form.Control
-                  onChange={(e) => {
-                    setAddress(e.target.value);
-                  }}
-                  as="textarea"
-                  rows={3}
-                />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleSave}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
+      <EmployeeModal data={null} toShow={toShowEmployeePopup} onClose={closeEmployeeClickHandler}/>
+      <div>
+        <Button onClick={addEmployeeClickHandler}>add employee</Button>
       </div>
-      
-        {employee.length === 0
-          ? <div className="empty">
-          <p>Nothing here</p>
-          <img className="image" src="https://cdn.dribbble.com/users/453325/screenshots/5573953/empty_state.png" alt=""/>
-
-          </div>
-          : <div className="home_container">{
-          employee.map((data, index) => {
-              return <EmployeeCard key={index} data={data} />;
-            })
-          }
-
-            </div>
-        }
-     
+     {toRender}
     </>
   );
 }
