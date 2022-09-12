f11b57dea024eda77b30ac372c1ede9cee896935
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-09-12 22:41:43 +0530

    removed strictmode because of twice rendering

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { EmployeeProvider } from "./context/employee-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Router>
       <AuthProvider>
         <EmployeeProvider>
@@ -17,7 +17,7 @@ root.render(
         </EmployeeProvider>
       </AuthProvider>
     </Router>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
