5442aa6294c1fc956384702e10d1daa4888a72b4
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-09-13 01:42:26 +0530

    handled error: not signed in

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import login from "../assests/login.jpg";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/auth-context";
-import { toast, ToastContainer } from "react-toastify";
 import "../style/signup.css";
 import "react-toastify/dist/ReactToastify.min.css";
+import { toast, ToastContainer } from "react-toastify";
 
 function Login() {
-  const { user, error, dispatch } = useAuth();
+  const { user, error, message, dispatch } = useAuth();
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const uploadData = (event) => {
-    event.preventDefault();
-    if (email === user.email && password === user.password) {
-      localStorage.setItem("user", JSON.stringify(user));
-      navigate("/home");
-    } else {
-      dispatch({
-        type: "AUTH_FAIL",
-        payload: "Email or password entered is wrong",
-      });
-    }
+  useEffect(() => {
     if (error) {
       toast.error(`${error}`, {
         position: "top-right",
@@ -33,6 +23,32 @@ function Login() {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
+      });
+    }else if(message){
+      toast.success(`${message}`, {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  }, [error, message]);
+
+  const uploadData = (event) => {
+    event.preventDefault();
+
+    if(!user){
+      dispatch({type:"AUTH_FAIL", payload: 'user not signed in'})
+    }else if (email === user.email && password === user.password) {
+      localStorage.setItem("user", JSON.stringify(user));
+      navigate("/home");
+    } else {
+      dispatch({
+        type: "AUTH_FAIL",
+        payload: "Email or password entered is wrong",
       });
     }
   };
@@ -77,7 +93,6 @@ function Login() {
             <br />
           </div>
           <input className="register_btn" type="submit" value="Login"></input>
-          <button>Login</button>
         </form>
       </div>
 
