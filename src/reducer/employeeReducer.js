253eb442f1e8fcba47f734756ec4877ee05947e8
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-09-12 22:45:42 +0530

    corrected the logic in reducer

--- src/reducer/employeeReducer.js
+++ src/reducer/employeeReducer.js
@@ -1,21 +1,42 @@
 export const employeeReducer = (state, { type, payload }) => {
+  console.log("employee reducer :: ", type)
   switch (type) {
     case "ADD_EMP":
-      return {
-        ...state,
-        employee: [...state.employee, { ...payload }],
-      };
+      let toAddEmployeeId;
+      const employeeList = state.employee;
 
+      if (employeeList.length > 0) {
+        const lastAddedEmployee = state.employee[state.employee.length - 1];
+        const lastEmployeeId = lastAddedEmployee.id;
+        toAddEmployeeId = lastEmployeeId + 1;
+      } else {
+        toAddEmployeeId = 1;
+      }
+      return {
+        ...state, 
+        employee: [...state.employee,{...payload,id: toAddEmployeeId}],
+      };
     case "DLT_EMP":
+
       return {
         ...state,
-        employee: [...state.employee.filter((ele) => ele.email !== payload.email)],
+        employee: [...state.employee.filter((ele) => ele.id === payload.toDeleteId)],
       };
+      
     case "UPDATE":
-      return {
-        ...state,
-        employee: payload,
-      };
+      let index = state.employee.findIndex((ele) => ele.id === payload.id);
+      console.log(index);
+      if(index > -1){
+        const employeeList = [...state.employee];
+        employeeList[index] = {...payload};
+
+        return {
+          ...state,
+          employee: employeeList,
+        }
+      }else{
+        return {...state};
+      }
 
     default:
       throw new Error("Unhandled action type");
