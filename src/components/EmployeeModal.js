f31e3169ba51ff14301ba7dcb2dfd39b1874c1ad
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-09-13 00:32:13 +0530

    replaced bootstrap form with html form

--- src/components/EmployeeModal.js
+++ src/components/EmployeeModal.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
 import { useEmployee } from "../context/employee-context";
+import '../style/employeeModal.css'
 
 function EmployeeModal({ toShow, onClose, editEmployeeId }) {
   const { employee, empDispatch } = useEmployee();
@@ -29,7 +28,6 @@ function EmployeeModal({ toShow, onClose, editEmployeeId }) {
           address,
         },
       });
-      console.log("Add employee modal added");
       onClose();
     } else {
       empDispatch({
@@ -52,97 +50,98 @@ function EmployeeModal({ toShow, onClose, editEmployeeId }) {
   return (
     <div className="add_employee_container">
       <Modal show={toShow} onHide={onClose}>
+      <form onSubmit={handleSave}>
         <Modal.Header closeButton>
           <Modal.Title>Add employee details</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-                value={name}
-                type="text"
-                autoFocus
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                type="email"
-                value={email}
-                autoFocus
-              />
-            </Form.Group>
+        
+          <div className="modal_form">
+            <h5>Email</h5>
+            <input
+              type={email}
+              required
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              placeholder="Enter Email"
+            ></input>
 
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Date of birth :</Form.Label>
-              <Form.Control
-                onChange={(e) => {
-                  setDob(e.target.value);
-                }}
-                type="date"
-                value={dob}
-                autoFocus
-              />
-            </Form.Group>
+            <h5>Name</h5>
+            <input
+              type="text"
+              required
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              placeholder="Enter name"
+            ></input>
 
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Mobile No.</Form.Label>
-              <Form.Control
-                onChange={(e) => {
-                  setNumber(e.target.value);
-                }}
-                type="tel"
-                value={number}
-                autoFocus
-              />
-            </Form.Group>
+            <h5>Mobile No. </h5>
+            <input
+              type="tel"
+              required
+              value={number}
+              onChange={(e) => {
+                setNumber(e.target.value);
+              }}
+              placeholder="Enter Password"
+            ></input>
 
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>City</Form.Label>
-              <Form.Control
-                onChange={(e) => {
-                  setCity(e.target.value);
-                }}
-                type="text"
-                value={city}
-                autoFocus
-              />
-            </Form.Group>
+            <h5>Date of birth</h5>
+            <input
+              type="date"
+              value={dob}
+              onChange={(e) => {
+                setDob(e.target.value);
+              }}
+              placeholder="Enter date of birth"
+            ></input>
 
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Address</Form.Label>
-              <Form.Control
-                onChange={(e) => {
-                  setAddress(e.target.value);
-                }}
-                as="textarea"
-                value={address}
-                rows={3}
-              />
-            </Form.Group>
-          </Form>
+            <h5>Address</h5>
+            <input
+              required
+              type="textarea"
+              value={address}
+              onChange={(e) => {
+                setAddress(e.target.value);
+              }}
+              placeholder="Enter address"
+            ></input>
+
+            <h5>City</h5>
+            <input
+              type="text"
+              value={city}
+              onChange={(e) => {
+                setCity(e.target.value);
+              }}
+              placeholder="Enter company"
+            ></input>
+
+            <br />
+          </div>
+          
+        
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={onClose}>
+
+        <button className="login_btn" onClick={onClose}>
+          Close
+        </button>
+        <input className="register_btn" type="submit" value="Save changes"></input>
+
+          
+          {/* <Button variant="secondary" onClick={onClose}>
             Close
           </Button>
           <Button variant="primary" onClick={handleSave}>
             Save Changes
-          </Button>
+          </Button> */}
         </Modal.Footer>
+        </form>
       </Modal>
     </div>
   );
